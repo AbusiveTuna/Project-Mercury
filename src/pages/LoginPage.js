cf1e1be5247120473c8cd4b0ecd4d0a4ef76accd
Tuna <Tdb00096@gmail.com>
2023-06-02 10:40:20 -0400

    Login page configuration

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -15,28 +15,31 @@ function LoginPage() {
     // Add functionality for forgot password here in future
   }
 
+
   return (
     <div className="LoginPage">
-      <h1>Login Page</h1>
-      <div>
-        <input 
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <input 
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="button-container">
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handleForgotPassword}>Forgot Password?</button>
+      <div className="LoginPage-content">
+        <h1>Login Page</h1>
+        <div>
+          <input 
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <button onClick={handleLogin}>Login</button>
+          <button onClick={handleForgotPassword}>Forgot Password?</button>
+        </div>
       </div>
     </div>
   );
