2c91cd98a87e459d902d7752f470ee32433e85a2
Tuna <Tdb00096@gmail.com>
2023-06-02 10:06:11 -0400

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
+// src/App.js
+
+import React from 'react';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = () => {
-    setUsername("");
-    setPassword("");
-  }
-
-  const handleForgotPassword = () => {
-    console.log("Forgot password button clicked");
-    // Add functionality for forgot password here in future
-  }
-
   return (
     <div className="App">
-      <h1>Project Mercury</h1>
-      <div>
-        <input 
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <input 
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="button-container">
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handleForgotPassword}>Forgot Password?</button>
-      </div>
+      <LoginPage />
     </div>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,45 @@
-import React from "react";
- 
-const Login = () => {
-    return (
-        <div>
-            <h1>
-                GeeksforGeeks is a Computer
-                Science portal for geeks.
-            </h1>
-        </div>
-    );
-};
- 
-export default About;
+import React, { useState } from 'react';
+import './login.css';
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    setUsername("");
+    setPassword("");
+  }
+
+  const handleForgotPassword = () => {
+    console.log("Forgot password button clicked");
+    // Add functionality for forgot password here in future
+  }
+
+  return (
+    <div className="App">
+      <h1>Project Mercury</h1>
+      <div>
+        <input 
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <input 
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="button-container">
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleForgotPassword}>Forgot Password?</button>
+      </div>
+    </div>
+  );
+}
+
+export default login;
