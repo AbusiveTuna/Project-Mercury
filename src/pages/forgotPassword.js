cbcb3d833073cd0ac9173f0b8ac8788a8e9dc732
Tuna <Tdb00096@gmail.com>
2023-06-04 01:05:45 -0400

    register and forgot pages

--- src/pages/forgotPassword.js
+++ src/pages/forgotPassword.js
@@ -1,7 +1,33 @@
-import React from 'react';
+// ForgotPassword.js
+import React, { useState } from 'react';
+import './ForgotPassword.css';
 
 function forgotPassword() {
-  return <h1>TODO</h1>;
+  const [email, setEmail] = useState("");
+
+  const handleResetPassword = () => {
+    setEmail("");
+  }
+
+  return (
+    <div className="ForgotPassword">
+      <div className="ForgotPassword-content">
+        <h1>Forgot your password?</h1>
+        <p>Enter your email below, if an account is found registered to that email you will be sent a link to reset your password</p>
+        <div>
+          <input 
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <button onClick={handleResetPassword}>Reset Password</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default forgotPassword;
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Register.css';
 
 function register() {
-  return <h1>TODO</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleRegister = () => {
+    if (email !== confirmEmail) {
+      setErrorMessage("Emails do not match");
+      return;
+    }
+    if (password !== confirmPassword) {
+      setErrorMessage("Passwords do not match");
+      return;
+    }
+
+    setUsername("");
+    setEmail("");
+    setConfirmEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setBirthDate("");
+    setErrorMessage("");
+
+    //add actual registration functionality here
+  }
+
+  return (
+    <div className="Register">
+      <div className="Register-content">
+        <h1>Register</h1>
+        <div>
+          <input 
+            type="text"
+            placeholder="Enter Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="email"
+            placeholder="Confirm Email"
+            value={confirmEmail}
+            onChange={e => setConfirmEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={e => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input 
+            type="date"
+            value={birthDate}
+            onChange={e => setBirthDate(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <button onClick={handleRegister} disabled={email !== confirmEmail || password !== confirmPassword}>Sign Up</button>
+        </div>
+        {errorMessage && <p>{errorMessage}</p>}
+      </div>
+    </div>
+  );
 }
 
 export default register;
