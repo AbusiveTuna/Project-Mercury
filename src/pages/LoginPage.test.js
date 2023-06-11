10fe7a0ead3c67d5409632464d63a22fed0c274a
Tuna <Tdb00096@gmail.com>
2023-06-11 14:36:41 -0400

    setting up testing

---
+++ src/pages/LoginPage.test.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
+import LoginPage from './LoginPage';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('./css/LoginPage.css', () => ({}));
+
+test('renders login page', () => {
+    act(() => {
+        render(
+          <MemoryRouter>
+            <LoginPage />
+          </MemoryRouter>
+        );
+      });
+  const usernameInput = screen.getByPlaceholderText('Username');
+  const passwordInput = screen.getByPlaceholderText('Password');
+  const loginButton = screen.getByText('Login');
+
+  expect(usernameInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(loginButton).toBeInTheDocument();
+});
+
+test('handles login', () => {
+    act(() => {
+        render(
+          <MemoryRouter>
+            <LoginPage />
+          </MemoryRouter>
+        );
+      });
+  const usernameInput = screen.getByPlaceholderText('Username');
+  const passwordInput = screen.getByPlaceholderText('Password');
+  const loginButton = screen.getByText('Login');
+
+  // Mock the fetch function
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ token: 'mockToken' }),
+    })
+  );
+
+  fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+  fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+
+  fireEvent.click(loginButton);
+
+  // Check if the fetch function was called with the correct arguments
+  expect(fetch).toHaveBeenCalledWith(
+    'https://protected-badlands-72029.herokuapp.com/login',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: 'testuser', password: 'testpassword' }),
+    }
+  );
+});
+
+test('displays error message for invalid login', async () => {
+    act(() => {
+        render(
+          <MemoryRouter>
+            <LoginPage />
+          </MemoryRouter>
+        );
+      });
+  const usernameInput = screen.getByPlaceholderText('Username');
+  const passwordInput = screen.getByPlaceholderText('Password');
+  const loginButton = screen.getByText('Login');
+
+  // Mock the fetch function to simulate an error response
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: false,
+    })
+  );
+
+  fireEvent.change(usernameInput, { target: { value: 'testuser' } });
+  fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+
+  fireEvent.click(loginButton);
+
+  const errorMessage = await screen.findByText('Invalid username or password');
+  expect(errorMessage).toBeInTheDocument();
+});
---
+++ src/pages/Register.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
+import Register from './Register';
+import { MemoryRouter } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('./css/Register.css', () => ({}));
+
+test('renders register page', () => {
+    act(() => {
+        render(
+          <MemoryRouter>
+            <Register />
+          </MemoryRouter>
+        );
+      });
+  const usernameInput = screen.getByPlaceholderText('Enter Username');
+  const emailInput = screen.getByPlaceholderText('Enter Email');
+  const confirmEmailInput = screen.getByPlaceholderText('Confirm Email');
+  const passwordInput = screen.getByPlaceholderText('Enter Password');
+  const confirmPasswordInput = screen.getByPlaceholderText('Confirm Password');
+  const birthDateInput = screen.getByPlaceholderText('Enter Birth Date');
+  const signUpButton = screen.getByText('Sign Up');
+
+  expect(usernameInput).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(confirmEmailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+  expect(confirmPasswordInput).toBeInTheDocument();
+  expect(birthDateInput).toBeInTheDocument();
+  expect(signUpButton).toBeInTheDocument();
+});
+
+test('displays mismatch class when email and confirm email do not match', () => {
+    act(() => {
+        render(
+          <MemoryRouter>
+            <Register />
+          </MemoryRouter>
+        );
+      });
+  const emailInput = screen.getByPlaceholderText('Enter Email');
+  const confirmEmailInput = screen.getByPlaceholderText('Confirm Email');
+
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+  fireEvent.change(confirmEmailInput, { target: { value: 'mismatch@example.com' } });
+
+  expect(emailInput).toHaveClass('mismatch');
+  expect(confirmEmailInput).toHaveClass('mismatch');
+});
+
+test('displays mismatch class when password and confirm password do not match', () => {
+    act(() => {
+        render(
+          <MemoryRouter>
+            <Register />
+          </MemoryRouter>
+        );
+      });
+  const passwordInput = screen.getByPlaceholderText('Enter Password');
+  const confirmPasswordInput = screen.getByPlaceholderText('Confirm Password');
+
+  fireEvent.change(passwordInput, { target: { value: 'password123' } });
+  fireEvent.change(confirmPasswordInput, { target: { value: 'mismatch123' } });
+
+  expect(passwordInput).toHaveClass('mismatch');
+  expect(confirmPasswordInput).toHaveClass('mismatch');
+});
