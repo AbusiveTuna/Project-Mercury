f421a3aa953d11eb563191177e7760d42e9e40e0
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-06-20 09:55:51 -0400

    adding loading spinner, and enter key press for submit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/LoginPage.css';
+import Spinner from 'react-bootstrap/Spinner'; //npm install react-bootstrap bootstrap
 
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
-  
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = () => {
+    setIsLoading(true);
     fetch('https://protected-badlands-72029.herokuapp.com/login', {
       method: 'POST',
       headers: {
@@ -25,10 +27,11 @@ function LoginPage() {
       }
     })
     .then(data => {
+      setIsLoading(false);
       navigate("/dashboard");
     })
     .catch(error => {
-      console.error('Error:', error);
+      setIsLoading(false);
       setErrorMessage('Invalid username or password');
     });
   };
@@ -40,34 +43,53 @@ function LoginPage() {
   const handleRegister = () => {
     navigate("/register");
   }
-
-  return (
+  
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && username && password) {
+      handleLogin();
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener('keypress', handleKeyPress);
+    return () => {
+      window.removeEventListener('keypress', handleKeyPress);
+    }
+  }, [username, password]);
+  
+ return (
     <div className="LoginPage">
       <div className="LoginPage-content">
-        <div>
-          <input 
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <input 
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="button-container">
-          <button onClick={handleLogin}>Login</button>
-        </div>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <div className="text-links">
-          <span className="forgot-password" onClick={handleForgotPassword}>Forgot Password?</span>
-          <span className="register" onClick={handleRegister}>New User? Register here</span>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <div>
+              <input 
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </div>
+            <div>
+              <input 
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="button-container">
+              <button onClick={handleLogin}>Login</button>
+            </div>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+            <div className="text-links">
+              <span className="forgot-password" onClick={handleForgotPassword}>Forgot Password?</span>
+              <span className="register" onClick={handleRegister}>New User? Register here</span>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
