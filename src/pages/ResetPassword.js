26ef81fc53971a2f9ebab05290471cf82d76e9c2
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-06-20 14:29:05 +0000

    forgot use callback

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 import './css/ResetPassword.css';
 
 function ResetPassword() {
