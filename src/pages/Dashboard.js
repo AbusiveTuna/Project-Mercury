100dcb1b2a0de51018e59a001d27064bbdfe0da4
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-03 07:42:59 -0400

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { BsGearFill } from 'react-icons/bs'; //need to install still?
 import './css/Dashboard.css';
 
 function Dashboard() {
-  return <h1>TODO</h1>;
+  return (
+    <>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          <BsGearFill />
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <h1>TODO</h1>
+    </>
+  );
 }
 
 export default Dashboard;
