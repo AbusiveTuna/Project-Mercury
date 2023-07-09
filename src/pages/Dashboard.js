0c60d0b950ed2425068b6961ebb5f5f8286044b1
Tuna <Tdb00096@gmail.com>
2023-07-09 14:33:53 -0400

    moving X to the right side

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -22,13 +22,14 @@ function Dashboard() {
         {!isSidebarOpen && <BsGearFill className="settings-button" onClick={() => setSidebarOpen(true)} />}
         <div className={isSidebarOpen ? "sidebar open" : "sidebar"}> 
           <BsX className="settings-button close-button" onClick={() => setSidebarOpen(false)} />
+          <div className="sidebar-content">
           <ul>
             <li>TODO 1</li>
             <li>TODO 2</li>
             <li>TODO 3</li>
           </ul>
         </div>
-
+        </div>
         <div className="graph-container">
           <BloodGlucoseGraph />
         </div>
