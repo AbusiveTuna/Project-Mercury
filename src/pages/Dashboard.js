19fcfd624f90f76ba606a7fbfa091ddb170afc22
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-17 09:50:01 -0400

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { BsGearFill, BsX } from 'react-icons/bs';
+import { BsGearFill } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
 import BloodGlucoseGraph from '../components/BloodGlucoseGraph';
 import CurrentBG from '../components/CurrentBG';
 import './css/Dashboard.css';
+import Settings from '../components/Settings';
 
 function Dashboard() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -17,11 +18,9 @@ function Dashboard() {
     navigate("/hueLightsLink");
   }
 
-  // Add state for the last blood glucose level and trend
   const [lastLevel, setLastLevel] = useState(null);
   const [lastTrend, setLastTrend] = useState(null);
 
-  // Add a function to update the last blood glucose level and trend
   const handleUpdateLastData = (level, trend) => {
     setLastLevel(level);
     setLastTrend(trend);
@@ -31,16 +30,7 @@ function Dashboard() {
     <>
       <div className="dashboard">
         {!isSidebarOpen && <BsGearFill className="settings-button" onClick={() => setSidebarOpen(true)} />}
-        <div className={isSidebarOpen ? "sidebar open" : "sidebar"}> 
-          <BsX className="settings-button close-button" onClick={() => setSidebarOpen(false)} />
-          <div className="sidebar-content">
-          <ul>
-            <li>TODO 1</li>
-            <li>TODO 2</li>
-            <li>TODO 3</li>
-          </ul>
-        </div>
-        </div>
+        <Settings isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className="graph-container">
           {lastLevel && lastTrend && (
             <CurrentBG level={lastLevel} trend={lastTrend} />
