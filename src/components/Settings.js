f0f22347f72f2d9d37ffd6ed4c9b5aa27b4161a2
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-17 09:47:51 -0400

    Create Settings.js

---
+++ src/components/Settings.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BsX } from 'react-icons/bs';
+
+function Settings({ isSidebarOpen, setSidebarOpen }) {
+  return (
+    <div className={isSidebarOpen ? "sidebar open" : "sidebar"}>
+      <BsX className="settings-button close-button" onClick={() => setSidebarOpen(false)} />
+      <div className="sidebar-content">
+        <ul>
+          <li>TODO 1</li>
+          <li>TODO 2</li>
+          <li>TODO 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Settings;
