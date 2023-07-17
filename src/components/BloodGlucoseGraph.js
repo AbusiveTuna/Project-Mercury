4eaf7680af334a55f3340f23067ceb5b3e8279f5
Tuna <Tdb00096@gmail.com>
2023-07-16 21:18:13 -0400

    updating with actual data

--- src/components/BloodGlucoseGraph.js
+++ src/components/BloodGlucoseGraph.js
@@ -11,13 +11,21 @@ const BloodGlucoseGraph = () => {
     const getDexcomData = async () => {
       try {
         const response = await fetch(`https://protected-badlands-72029.herokuapp.com/getDexcomData/${userId}`);
-        const data = await response.json();
-        console.log(data);
+        const rawData = await response.json();
+        console.log(rawData);
 
         if (response.ok) {
+          // Extract only the properties you care about from each item
+          const data = rawData.map(item => ({
+            displayTime: item.displayTime,
+            value: item.value,
+            trend: item.trend,
+            trendRate: item.trendRate,
+          }));
+
           setDexcomData(data);
         } else {
-          console.error(data.message);
+          console.error(rawData.message);
         }
       } catch (error) {
         console.error('Failed to fetch Dexcom data: ', error);
@@ -28,18 +36,32 @@ const BloodGlucoseGraph = () => {
   }, [userId]);
 
   const data = {
-    labels: dexcomData ? dexcomData.map(item => item.date) : [],
+    labels: dexcomData ? dexcomData.map(item => new Date(item.displayTime)) : [],
     datasets: [
       {
         label: 'Blood Glucose Level',
-        data: dexcomData ? dexcomData.map(item => item.glucoseLevel) : [],
+        data: dexcomData ? dexcomData.map(item => item.value) : [],
         fill: false,
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgba(255, 99, 132, 0.2)',
       },
+      {
+        label: 'Trend',
+        data: dexcomData ? dexcomData.map(item => item.trend) : [],
+        fill: false,
+        backgroundColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgba(75, 192, 192, 0.2)',
+      },
+      {
+        label: 'Trend Rate',
+        data: dexcomData ? dexcomData.map(item => item.trendRate) : [],
+        fill: false,
+        backgroundColor: 'rgb(153, 102, 255)',
+        borderColor: 'rgba(153, 102, 255, 0.2)',
+      },
     ],
   };
-  
+
   const options = {
     responsive: true,
     maintainAspectRatio: false,
@@ -49,8 +71,6 @@ const BloodGlucoseGraph = () => {
       },
     },
   };
-  
-
 
   return (
     <>
