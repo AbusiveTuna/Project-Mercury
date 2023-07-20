4978daa85dd7a171029d49ddd7543206f1b956eb
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-20 13:57:56 +0000

    updating trend

--- src/components/CurrentBG.js
+++ src/components/CurrentBG.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './css/CurrentBG.css';
+import { upload } from '@testing-library/user-event/dist/upload';
 
 const CurrentBG = ({ level, trend }) => {
+  let trendClass = '';
+  if (trend === '90down') {
+    trendClass = 'trend-down';
+  } else if (trend === '90up') {
+    trendClass = 'trend-up';
+  } else if (trend === '45up') {
+    trendClass = 'trend-up-right';
+  } else if (trend === '45down') {
+    trendClass = 'trend-down-right';
+  }
+  else{
+    trendClass = 'trend-up';
+  }
+
   return (
     <div className="blood-glucose-level-container">
-      <div className="level">{level}</div>
-      <div className="trend">{trend}</div>
+      <div className="level-container">
+        <div className="level">{level}</div>
+        <div className={`trend ${trendClass}`}></div>
+      </div>
     </div>
   );
 };
