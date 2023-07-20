07b4b792643f18353019391b740efe3571ba09e5
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-20 13:24:29 +0000

    fixing router

--- src/tests/components/DexcomRedirect.test.js
+++ src/tests/components/DexcomRedirect.test.js
@@ -2,42 +2,36 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import DexcomRedirect from '../../components/DexcomRedirect'
 import Cookies from 'js-cookie';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 jest.mock('js-cookie');
 
 describe('DexcomRedirect', () => {
   it('renders Redirecting... text', () => {
-    const history = createMemoryHistory();
     render(
-      <Router history={history}>
+      <MemoryRouter>
         <DexcomRedirect />
-      </Router>
+      </MemoryRouter>
     );
     expect(screen.getByText('Redirecting...')).toBeInTheDocument();
   });
 
   it('renders error message when state does not match expected state', () => {
-    const history = createMemoryHistory();
-    history.push('/?state=wrongState');
     Cookies.get.mockReturnValueOnce('user_id').mockReturnValueOnce('dexcomState');
     render(
-      <Router history={history}>
+      <MemoryRouter initialEntries={['/?state=wrongState']}>
         <DexcomRedirect />
-      </Router>
+      </MemoryRouter>
     );
     expect(screen.getByText('State mismatch error')).toBeInTheDocument();
   });
 
   it('renders error message when no auth code is found', () => {
-    const history = createMemoryHistory();
-    history.push('/?state=dexcomState');
     Cookies.get.mockReturnValueOnce('user_id').mockReturnValueOnce('dexcomState');
     render(
-      <Router history={history}>
+      <MemoryRouter initialEntries={['/?state=dexcomState']}>
         <DexcomRedirect />
-      </Router>
+      </MemoryRouter>
     );
     expect(screen.getByText('No auth code found')).toBeInTheDocument();
   });
