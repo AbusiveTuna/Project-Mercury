8cb20f83718a4bbb4f26d5cb8b5dff1bb9a0d4d5
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-07-24 08:32:44 -0400

    Update WarningThresholds.test.js

--- src/tests/components/WarningThresholds.test.js
+++ src/tests/components/WarningThresholds.test.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import WarningThresholds from '../WarningThresholds';
+import '@testing-library/jest-dom/extend-expect';
 
+describe('WarningThresholds', () => {
+  /* 
+  * Test Name: Render high and low threshold inputs
+  * Unit Test ID: CUTX
+  * Description: Tests rendering of high and low threshold inputs
+  */
+  it('should render high and low threshold inputs', () => {
+    render(<WarningThresholds />);
+    const highThresholdInput = screen.getByLabelText('High Threshold:');
+    const lowThresholdInput = screen.getByLabelText('Low Threshold:');
+    expect(highThresholdInput).toBeInTheDocument();
+    expect(lowThresholdInput).toBeInTheDocument();
+  });
+
+  /* 
+  * Test Name: Update high threshold value
+  * Unit Test ID: CUTX
+  * Description: Tests updating of high threshold value when input is changed
+  */
+  it('should update high threshold value when input is changed', () => {
+    render(<WarningThresholds />);
+    const highThresholdInput = screen.getByLabelText('High Threshold:');
+    fireEvent.change(highThresholdInput, { target: { value: '400' } });
+    expect(highThresholdInput.value).toBe('400');
+  });
+
+  /* 
+  * Test Name: Update low threshold value
+  * Unit Test ID: CUTX
+  * Description: Tests updating of low threshold value when input is changed
+  */
+  it('should update low threshold value when input is changed', () => {
+    render(<WarningThresholds />);
+    const lowThresholdInput = screen.getByLabelText('Low Threshold:');
+    fireEvent.change(lowThresholdInput, { target: { value: '50' } });
+    expect(lowThresholdInput.value).toBe('50');
+  });
+});
