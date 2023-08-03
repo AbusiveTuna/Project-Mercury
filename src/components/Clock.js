e1bccbf1ad0567ba10236487f74670f8c4c4e699
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-08-03 13:30:41 -0400

    Create Clock.js

---
+++ src/components/Clock.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    return `${hours}:${minutes} ${ampm}`;
+  };
+
+  return <div>{formatTime(time)}</div>;
+}
+
+export default Clock;
