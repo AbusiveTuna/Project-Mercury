44a5c4920a54e6e9e00493e8ba8f68d17246fa4a
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-08-03 13:09:09 -0400

    Update WarningThresholds.js

--- src/components/WarningThresholds.js
+++ src/components/WarningThresholds.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './css/WarningThresholds.css';
 
@@ -18,7 +18,7 @@ const WarningThresholds = ({ lowThreshold, setLowThreshold, highThreshold, setHi
     };
 
     getUserSettings();
-  }, [userId]);
+  }, [userId, setHighThreshold, setLowThreshold]);
 
   const handleUpdateUserSettings = async () => {
     try {
