82d2faf45e066221de3ebccb32d93d6fa65c8a40
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-08-03 07:47:52 -0400

    Update Alerts.js

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { highThreshold, lowThreshold } from './WarningThresholds';
 import { level, trend } from './CurrentBG';
+import { devices } from './HueLightsSettings';
+
 
 
 function Alerts() {
+  const [checkedDevices, setCheckedDevices] = useState([]);
 
 }
+
+export default Alerts;
