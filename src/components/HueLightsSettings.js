06bc9349f426619638ea390b5239bb2e7ad4f04b
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2023-08-03 07:28:01 -0400

    Update HueLightsSettings.js

--- src/components/HueLightsSettings.js
+++ src/components/HueLightsSettings.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { BsX } from 'react-icons/bs';
+import './css/Settings.css';
 
+function HueLightsSettings({ isSidebarOpen, setSidebarOpen }) {
+  return (
+    <div className={isSidebarOpen ? "sidebar open" : "sidebar"} data-testid="sidebar">
+      <BsX className="settings-button close-button" onClick={() => setSidebarOpen(false)} data-testid="close-button" />
+      <div className="sidebar-content">
+        <ul>
+          <li>TODO 1</li>
+          <li>TODO 2</li>
+          <li>TODO 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default HueLightsSettings;
